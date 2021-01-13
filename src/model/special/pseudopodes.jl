33b2fb088e1d671cf3341c84efa27391e1b5eaf6
Gabriel Torregrosa <g.torregrosa@outlook.com>
2021-01-13 16:38:22 +0100

    Added addPseudopode!

--- src/model/special/pseudopodes.jl
+++ src/model/special/pseudopodes.jl
@@ -24,34 +24,39 @@ sqrt((x₁-x₂)^2+(y₁-y₂)^2) < 2.
 "
 force = 
 "
-f = sqrt((x₁-x₂)^2+(y₁-y₂)^2)*exp(-sqrt((x₁-x₂)^2+(y₁-y₂)^2))
+sqrt((x₁-x₂)^2+(y₁-y₂)^2)*exp(-sqrt((x₁-x₂)^2+(y₁-y₂)^2))
 "
 addPseudopode!(m,:f,condition,force)
 ```
 """
-function addPseudopode!(agentModel::Model, symbol::Symbol, condition::String, force::String; randVar = Tuple{Symbol,String}[])
+function addPseudopode!(agentModel::Model, var::Symbol, condition::String, force::String; randVar = Tuple{Symbol,String}[])
     
+    #Checks
+    condition = Meta.parse(condition)
+    force = Meta.parse(force)
     #Check if a Pseudopode force already exists
     if Pseudopode in [typeof(i) for i in agentModel.special]
         error("A pseudopode force is already present in the model. Only one pseudopode force can exist in the model.")
     end
-
-    #Check integrity of the inputs
-    condition = splitUpdating(condition)
-    if length(condition) != 1
-        error("Error in the pseudopode condition definition.")
-    end 
-    force = splitUpdating(force)
-    if length(force) != 1
-        error("Error in the pseudopode force definition.")
-    end 
-
+    #Check var
+    checkDeclared(agentModel, var)
+    #Check random variables
+    checkRandDeclared(agentModel, randVar)
     #Add necessary variables
     addIfNot!(agentModel.declaredIds, [:id_,:nNPseudo_,:pseudoId_])
 
-    #Check random variables
-    checkRandDeclared(agentModel, randVar)
+    eqs = copy(agentModel.inter)
+    
+    push!(eqs,
+    :(if nNPseudo_₁ == id_₂; 
+            $(Meta.parse(string(var,"₁"))) += $force
+            $(Meta.parse(string(var,"₂"))) += -$(Meta.parse(string(var,"₁")))
+        end)
+    )
 
+    agentModel.inter = eqs
+    push!(agentModel.declaredSymb["inter"],var)
 
+    return
     
 end
