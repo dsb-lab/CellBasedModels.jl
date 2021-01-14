8a54b97253f12c17e7b1703fd65763e62ecf8897
Gabriel Torregrosa <g.torregrosa@outlook.com>
2021-01-14 16:33:12 +0100

    addInteraction! 2.0

--- src/model/basic/addInteraction.jl
+++ src/model/basic/addInteraction.jl
@@ -21,29 +21,14 @@ function addInteraction!(agentModel::Model, addvar::Symbol, addeqs::String)
 
     agentModel.evolve = needCompilation
     
-
-    newEqs = splitUpdating(addeqs)
-    if length(newEqs) == 0
-        error("An equation has to be defined for the declared variable ", addvar, ".")
-    elseif length(newEqs) > 1
-        error("Interaction parameter ", addvar, " associted with more than interaction rule.")
-    end
-    #Check repeated declarations
-    varsDeclared=Meta.parse(string(Meta.parse(newEqs[1]).args[1])[1:end-1])
-    index = string(Meta.parse(newEqs[1]).args[1])[end]
-    if index != '₁' #Check index declared
-        error("""Interaction parameter """, Meta.parse(string(Meta.parse(newEqs[1]).args[1])), """ has to be declared with underscript "₁".""")
-    end
-    if addvar != varsDeclared
-        error("Interaction parameter ", addvar, " but interaction rule for variable ", varsDeclared, " declared.")
-    end
+    newEqs = Meta.parse(string("begin \n", addeqs, " \nend"))
     
     eqs = copy(agentModel.inter)
     
     #Check vars except RESERVEDVARS
     checkDeclared(agentModel,addvar)
 
-    push!(eqs,Meta.parse([j for j in newEqs if contains(j,string(addvar,"₁"))][1]))
+    push!(eqs,newEqs)
 
     agentModel.inter = eqs
     push!(agentModel.declaredSymb["inter"],addvar)
@@ -76,46 +61,7 @@ function addInteraction!(agentModel::Model, addvar::Array{Symbol}, addeqs::Strin
 
     agentModel.evolve = needCompilation
         
-    newEqs = splitUpdating(addeqs)
-    #Check repeated declarations
-    varsDeclared = []
-    for i in newEqs
-        push!(varsDeclared,Meta.parse(string(Meta.parse(i).args[1])[1:end-1]))
-        index = string(Meta.parse(i).args[1])[end]
-        if index != '₁' #Check index declared
-            error("""Interaction parameter """, Meta.parse(string(Meta.parse(i).args[1])), """ has to be declared with underscript "₁".""")
-        end
-    end
-    for i in varsDeclared
-        if length(findall(varsDeclared.==i))>1
-            error("Interaction parameter ", i, " declared with more than interaction rule.")
-        end
-    end
-    for i in addvar
-        if length(findall(addvar.==i))>1
-            error("Parameter ", i, " declared with more than one update rule.")
-        end
-    end
-    #Check same declarations for vars and eqs
-    if length(varsDeclared) <= length(addvar)
-        notDeclared = []
-        for i in addvar
-            if !(i in varsDeclared)
-                push!(notDeclared,i)
-            end
-        end
-        if length(notDeclared) > 0
-            error("Declared interaction parameters with no associated interaction rule: $(notDeclared...) ")
-        end
-    elseif length(varsDeclared) > length(addvar)
-        notDeclared = []
-        for i in varsDeclared
-            if !(i in keys(addvar))
-                push!(notDeclared,i)
-            end
-        end
-        error("Interaction parameters with associated interaction rule but not declared: $(notDeclared...)")        
-    end
+    newEqs = Meta.parse(string("begin ", addeqs, " end"))
         
     eqs = copy(agentModel.inter)
     
@@ -123,9 +69,8 @@ function addInteraction!(agentModel::Model, addvar::Array{Symbol}, addeqs::Strin
     for i in addvar
         checkDeclared(agentModel,i)
     end
-    for i in addvar
-        push!(eqs,Meta.parse([j for j in newEqs if contains(j,string(i,"₁"))][1]))
-    end
+
+    push!(eqs,newEqs)
 
     agentModel.inter = eqs
     append!(agentModel.declaredSymb["inter"],addvar)
