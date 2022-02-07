fd4712bb4c79f1fb43ec423448a47606edd1e759
Gabriel Torregrosa <g.torregrosa@outlook.com>
2022-02-07 17:33:29 +0100

    Make exponentialCUDA consistent with Distributions

--- src/model/random/distribution.jl
+++ src/model/random/distribution.jl
@@ -4,4 +4,4 @@ VALIDDISTRIBUTIONSCUDA = [:Normal,:Uniform,:Exponential]
 #Random distribution transformations for cuda capabilities
 NormalCUDA(x,μ,σ) = σ*CUDA.sqrt(2.)*SpecialFunctions.erfinv(2*(x-.5))+μ
 UniformCUDA(x,l0,l1) = (l1-l0)*x+l0
-ExponentialCUDA(x,λ) = -CUDA.log(1-x)/λ
+ExponentialCUDA(x,θ) = -CUDA.log(1-x)*θ
