95b5d181547ee2772b02775c683cb4bd0733d8f5
Gabriel Torregrosa <g.torregrosa@outlook.com>
2022-03-01 16:04:14 +0100

    Uncomment tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,18 +14,17 @@ end
 
 # testplatforms = ["cpu"]
 
-# include("testAgent.jl")
-# include("testSubstitution.jl")
-# include("testAuxiliar.jl")
-# include("testModel.jl")
-# include("testRandom.jl")
-# include("testCommunity.jl")
-# include("testUpdates.jl")
-# include("testIntegrator.jl")
-# include("testEvent.jl")
-# include("testNeighbors.jl")
-# include("testSave.jl")
-# include("testOptimization.jl")
-
+include("testAgent.jl")
+include("testSubstitution.jl")
+include("testAuxiliar.jl")
+include("testModel.jl")
+include("testRandom.jl")
+include("testCommunity.jl")
+include("testUpdates.jl")
+include("testIntegrator.jl")
+include("testEvent.jl")
+include("testNeighbors.jl")
+include("testSave.jl")
+include("testOptimization.jl")
 include("testMedium.jl")
 
