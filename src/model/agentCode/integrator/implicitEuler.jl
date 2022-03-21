6e6f1e418eb92359ec6cd4a24d3ead166553cffd
Gabriel Torregrosa <g.torregrosa@outlook.com>
2022-03-21 18:37:06 +0100

    Add comments to the implicit Euler

--- src/model/agentCode/integrator/implicitEuler.jl
+++ src/model/agentCode/integrator/implicitEuler.jl
@@ -93,9 +93,9 @@ function addIntegratorImplicitEuler_!(p::Program_, platform::String)
                     while errorMax > relativeErrorIntegrator && maxLearningStepsIntegrator > count
                         $(addInteraction...)
                         @platformAdapt integrationStep1_!(ARGS_)
+                        localVCopy .= (1-learningRateIntegrator).*localVCopy .+ learningRateIntegrator.*predV
                         errorMax = maximum(abs.(predV .- localVCopy))
                         println(errorMax)
-                        localVCopy .= (1-learningRateIntegrator).*localVCopy .+ learningRateIntegrator.*predV
 
                         #println(localVCopy[1,:])
                         count += 1
