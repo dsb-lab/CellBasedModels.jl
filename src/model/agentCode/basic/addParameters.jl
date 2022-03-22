ad88e5485c52ad4331d179aa2248b82004104953
Gabriel Torregrosa <g.torregrosa@outlook.com>
2022-03-22 12:59:11 +0100

    Add comments to the implicit Euler

--- src/model/agentCode/basic/addParameters.jl
+++ src/model/agentCode/basic/addParameters.jl
@@ -265,7 +265,6 @@ function addCopyInitialisation_!(p::Program_,platform::String)
     end
 
     if create
-        println(p.update)
         gen = quote end #General update
         up = quote end
         for i in keys(p.agent.declaredSymbols)
