6bb54549dc4e9bbb04d77a16d8372e570ba42661
Gabriel Torregrosa <g.torregrosa@outlook.com>
2022-04-29 11:34:16 +0200

    Improve speed GPU saving to RAM

--- src/model/agentCode/saving/saveRAM.jl
+++ src/model/agentCode/saving/saveRAM.jl
@@ -3,45 +3,44 @@ function addSavingRAM_!(p::Program_,platform::String)
     #List of nonempty arrays
     l = []
 
-    if length(p.agent.declaredSymbols["Local"])>0
-        push!(l,:(Core.Array(localV[1:N,:])))
+    if length(keys(p.agent.declaredSymbols["Local"]))>0
+        push!(l,:(Core.Array(view(localV,1:N,:))))
     else
         push!(l,:(Core.Array{Float64,2}(undef,0,2)))
     end
 
-    if length(p.agent.declaredSymbols["LocalInteraction"])>0
-        push!(l,:(Core.Array(localInteractionV[1:N,:])))
+    if length(keys(p.agent.declaredSymbols["LocalInteraction"]))>0
+        push!(l,:(Core.Array(view(localInteractionV,1:N,:))))
     else
         push!(l,:(Core.Array{Float64,2}(undef,0,2)))
     end
 
-    if length(p.agent.declaredSymbols["Identity"])>0
-        push!(l,:(Core.Array(identityV[1:N,:])))
+    if length(keys(p.agent.declaredSymbols["Identity"]))>0
+        push!(l,:(Core.Array(view(identityV,1:N,:))))
     else
         push!(l,:(Core.Array{Int,2}(undef,0,2)))
     end
 
-    if length(p.agent.declaredSymbols["IdentityInteraction"])>0
-        push!(l,:(Core.Array(identityInteractionV[1:N,:])))
+    if length(keys(p.agent.declaredSymbols["IdentityInteraction"]))>0
+        push!(l,:(Core.Array(view(identityInteractionV,1:N,:))))
     else
         push!(l,:(Core.Array{Int,2}(undef,0,2)))
     end
 
-    if length(p.agent.declaredSymbols["Global"])>0
+    if length(keys(p.agent.declaredSymbols["Global"]))>0
         push!(l,:(Core.Array(globalV)))
     else
         push!(l,:(Core.Array{Float64,1}()))
     end
 
-    if length(p.agent.declaredSymbols["GlobalArray"]) > 0
-        list = string("[copy(",p.agent.declaredSymbols["GlobalArray"][1],"")
-        list = string(list,")]")
+    if length(keys(p.agent.declaredSymbols["GlobalArray"])) > 0
+        list = string("[",[string("copy(",i,")") for i in keys(p.agent.declaredSymbols["GlobalArray"])]...,"]")
         push!(l,Meta.parse(string("Core.Array(",list,")")))
     else
         push!(l,:(Core.Array{Core.Array{Float64},1}()))
     end
 
-    if length(p.agent.declaredSymbols["Medium"]) > 0
+    if length(keys(p.agent.declaredSymbols["Medium"])) > 0
         if p.agent.dims == 1
             push!(l,:(Core.Array(mediumV)))
         elseif p.agent.dims == 2
