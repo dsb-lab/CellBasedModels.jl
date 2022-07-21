c93a86b1a8ffab338d525c56cf9d78be71da0494
Gabriel Torregrosa <g.torregrosa@outlook.com>
2022-07-21 17:54:58 +0200

    Add timing in evolve

--- src/agentCompiled/agentCode/saving/saveRAM.jl
+++ src/agentCompiled/agentCode/saving/saveRAM.jl
@@ -91,14 +91,14 @@ function addSavingRAM_!(p::Program_,platform::String)
 
     push!(p.execInloop.args,
         :(begin
-            if countSave_ == nSave_ && $checkNMax
+            @time if countSave_ == nSave_ && $checkNMax
                 countSave_ = 1
                 ob = Community($(p.agent.dims),t+dt,N,com.mediumN,com.simulationBox,com.radiusInteraction,com.declaredSymbols_,$(l...))
                 push!(commRAM_,ob)
 
                 timeEnd_ = time()
                 println("Iteration: ",step_,"/",nSteps_)
-                println("Elapsed time: ",timeEnd_ - timeStart_, " seconds. Nunber of agents: ", N, ".\n")
+                println("Elapsed time: ",timeEnd_ - timeStart_, " seconds. Number of agents: ", N, ".\n")
                 timeStart_ = timeEnd_
             else
                 countSave_ += 1
